21bac084a5a580be6a64258127c1c87bada193f6
LUCAS MATHEUS DE SOUZA <lucas.matheus@redeunifique.com.br>
2023-02-02 14:13:20 -0300

    Criando novas inputs e definindo a função batalha

--- RPG.py
+++ RPG.py
@@ -1,10 +1,12 @@
-print("Olá, imagine que você passou um longo dia no trabalho.\n")
+print("\n")
+print("Imagine que você passou um longo dia no trabalho.\n")
 print("Chegando em casa, após todas as suas obrigações você se vê cansado o bastante e decide descansar.\n")
 print("Sua cabeça gira frenéticamente ao longo de um grande pesadelo.\n")
 print("Você acorda então...\n")
 print(41*"*")
 print(3*"*","No labirinto da sua mente cansada",3*"*")
 print(41*"*")
+print("\n")
 
 guerreiro = input("Qual o seu nome ? ")
 print("\n")
@@ -29,7 +31,7 @@ while True:#laço para que as entradas sejam validadas
     print("\n")
 
 
-    escolha = input("1 ou 2 ?" )
+    escolha = input("1 ou 2 ? " )
 
     if escolha.isdigit():
         escolha = int(escolha)
@@ -50,8 +52,10 @@ while True:#laço para que as entradas sejam validadas
         
         else:
             print("Opção inválida. Por favor, escolha apenas opções listadas.")
+            print("\n") 
     else:
         print("Por favor digite apenas números.")
+        print("\n") 
 
 print(18*"*")
 print("\n")
@@ -68,6 +72,28 @@ print(41*"*")
 print("\n")
 print("Você fica com um certo receio após ouvir o barulho.\n""Mas se lembra que tem um isqueiro no bolso.\n")
 
+#definindofunções para a escolha 2
+dano_do_buraco = 6
+guerreiro_vida_total_queda = guerreiro_vida_total - dano_do_buraco
+
+def nao_acendeu():
+    print("Como você não acendeu o isqueiro não pode ver o grande buraco que estava a sua frente.\n")
+    print("Você enfia uma das pernas no buraco e acaba virando o pé.\n")
+    print("Com a dor {} solta um grito que ecoa em toda a caverna.".format(guerreiro))
+    print("Enquanto sente uma grande dor, você lembra do seu isqueiro e decide usar, porém com o impacto ele caiu de seu bolso.\n")
+    print("Frustrado, debilitado e com raiva você tem mais uma surpresa. . .\n")
+    print("{} possui agora {} pontos de vida.\n".format(guerreiro,guerreiro_vida_total_queda))
+    print("Pontos de vida? Você se questiona. Melhor ter mais cuidado daqui pra frente.")
+    
+    
+
+def acendeu():
+    print("Como você havia acendido o seu isqueiro, pode ver o grande buraco que estava logo a frente.\n")
+    print("Você contorna o buraco com muito cuidado.\n")
+    print("{} Pensa como é esperto em ter usado a pequena fonte de luz.\n".format(guerreiro))
+
+########
+
 while True:
     print("Gostaria de acender o seu isqueiro? ")
     print("Acender - 1\nNão acender - 2")
@@ -77,9 +103,16 @@ while True:
         isqueiro = int(isqueiro)
         if isqueiro == 1:
             print("Você vê o ambiente se iluminar após a pequena chama surgir.")
+            print("Enquanto ainda escuta barulhos, você decide ir mais a fundo pois sua curiosidade fala mais forte.\n")
+            print("Nada faz sentido, você segue tateando as paredes tentando se orientar.\n")
+            acendeu()
             break
         elif isqueiro == 2:
             print("Você decide não acender seu isqueiro\n")
+            print("Enquanto ainda escuta barulhos, você decide ir mais a fundo pois sua curiosidade fala mais forte.\n")
+            print("Nada faz sentido, você segue tateando as paredes tentando se orientar.\n")
+            
+            nao_acendeu()
             break
         else:
             print("Opção inválida. Por favor, escolha apenas opções listadas.")
@@ -87,10 +120,58 @@ while True:
         print("Por favor digite apenas números.")
 
 
-    
-            
-            
+print("Após a surpresa, seguindo mais a frente você percebe que o barulho havia desaparecido.\n ")
+print(" Ao longe na escuridão algo brilha, você sente um frio percorrer sua espinha, mas percebe que não tem outra opção a não ser seguir em frente. . .")
 
+#batalha e saída da caverna
+
+print("\n")
+print(39*"*")
+print(3*"*"," Capítulo 2 " "Coragem ou medo ?. ",3*"*")
+print(39*"*")
+print("\n")
+
+print("Avançando você percebe que o ambiente está ficando mais iluminado, tudo indica que é a saída.\n")
+print(f"{guerreiro} se alegra ao ver a saída, mas algo ainda não está correto. \n")
+print("Uma forma estranha está parada no caminho, o brilho que você havia visto anteriormente, são seus olhos. . . ")
+print("Você precisa enfrentar a criatura para sair.")
+
+goblin = 10
+inimigo = goblin
+
+#definindo a função de batalha
+
+def batalha(inimigo,guerreiro_vida_total):
+  while inimigo > 0 and guerreiro_vida_total > 0:
+    print("Deseja atacar - 1 \nCorrer - 2 ")
+    entrada = input("1 ou 2 ? ")
+    if entrada.isdigit():
+      entrada =int(entrada)
+      if entrada == 1:#laço de ataque
+        print("Você ataca sem piedade!")
+        hit = 1
+        inimigo -= hit
+        print(f"Você acertou com {hit} de dano! Agora o inimigo tem {inimigo} de vida. ")
+        if inimigo == 0:
+          print("Você venceu")#fim do laço de ataque        
+      elif entrada == 2:
+        print("Você tenta correr, mas não tem sucesso!")
+        hit =1
+        guerreiro_vida_total -=hit      
+        print(f"Na sua tentativa frustrada de fuga o inimigo te acerta com {hit} de dano!\n")
+        print(guerreiro_vida_total)
+        if guerreiro_vida_total == 0:
+          print("Sua aventura termina aqui!\n""Você acorda em sua cama pensando...\n")
+          print("Eu preciso chamar esse rapaz para uma entrevista! ") 
+      else:
+            print("Opção inválida. Por favor, escolha apenas opções listadas.")
+    else:
+        print("Por favor digite apenas números.")
+
+########            
+            
+#while true:
+ #  print("Você precisa lutar para sair.\n""Mas com sua afobação, {} precisa pensar no que fazer)
         
 
 
